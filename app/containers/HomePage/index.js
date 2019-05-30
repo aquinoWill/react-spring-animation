5739c10c897a6a12852e480b860747c20888b10f
Willian Aquino <willian.aquino@iterative.com.br>
2019-05-30 16:57:40 -0300

    Atualizando boiler plate

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,18 +1,41 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- */
-
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { useTransition, config } from 'react-spring';
+import {
+  Container,
+  Item,
+  Title,
+  Description,
+  Button,
+  Avatar,
+  Section,
+} from './styles';
+import data from './data';
 
-export default function HomePage() {
+const HomePage = () => {
+  const transitions = useTransition(data, item => item.id, {
+    from: { bottom: -200 },
+    enter: { bottom: 0 },
+    config: config.wobbly,
+  });
+
+  const src = 'https://farm8.staticflickr.com/7328/9000198669_57003f7505_n.jpg';
+
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <Section>
+      <Container>
+        {transitions.map(({ item, props, key }) => (
+          <Item key={key} style={props}>
+            <img src={src} alt="imagem" />
+
+            <Avatar src={item.avatar} alt="Avatar" />
+            <Title>{item.name}</Title>
+            <Description>{item.description}</Description>
+            <Button>View Profile</Button>
+          </Item>
+        ))}
+      </Container>
+    </Section>
   );
-}
+};
+
+export default HomePage;
